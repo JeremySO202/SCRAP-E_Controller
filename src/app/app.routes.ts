c0417dddbe2b52fef23341ecf5c09fada1fe132b
JeremySO202 <jeremyserracin@gmail.com>
2025-05-21 20:25:12 -0600

    Basic controller
    Control basico e idea de modos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ControllerComponent } from './controller/controller.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'controller', component: ControllerComponent },
+];
+
