1e607ab119e3ed5a0ae268dcd04fece5b741a639
JeremySO202 <jeremyserracin@gmail.com>
2025-05-22 10:02:01 -0600

    Modes implementation
    Modulo loop para instanciar distintos modulos(cambiar nombre despues)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./menu/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'SCRAP-E_Controller';
